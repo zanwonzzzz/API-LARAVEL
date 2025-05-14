52df415a4122117d0db528d2c2bdca5474c1fc07
Etni Castro <etnicastro9@gmail.com>
2025-05-14 12:19:16 -0600

    tercer commit

--- config/database.php
+++ config/database.php
@@ -82,6 +82,26 @@ return [
             ]) : [],
         ],
 
+         'mysql3' => [
+            'driver' => 'mysql',
+            'url' => env('DB_URL'),
+            'host' => env('DB_HOST3'),
+            'port' => env('DB_PORT'),
+            'database' => env('DB_DATABASE3'),
+            'username' => env('DB_USERNAME3'),
+            'password' => env('DB_PASSWORD3'),
+            'unix_socket' => env('DB_SOCKET', ''),
+            'charset' => env('DB_CHARSET', 'utf8mb4'),
+            'collation' => env('DB_COLLATION', 'utf8mb4_unicode_ci'),
+            'prefix' => '',
+            'prefix_indexes' => true,
+            'strict' => true,
+            'engine' => null,
+            'options' => extension_loaded('pdo_mysql') ? array_filter([
+                PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
+            ]) : [],
+        ],
+
         'mariadb' => [
             'driver' => 'mariadb',
             'url' => env('DB_URL'),
